817a0d9764a057349e6605abb1b20893cded62a3
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2022-06-24 00:35:01 -0300

    criadas telas de input e validações

--- Program.cs
+++ Program.cs
@@ -1,16 +1,96 @@
 ﻿using _854_ExercicioFinanceira_Sergio;
 
-//Teste Pessoa Física
-Console.WriteLine("Teste Pessoa Física");
-Console.WriteLine();
-DateTime nascimentoPf1 = new DateTime(1989, 06, 01);
-ContratoPessoaFisica contratoPf1 = new("José Silva", 10000, 10, "123.456.789-96", nascimentoPf1);
-contratoPf1.ExibirInfo();
+ControlaSistema();
 
-//Teste Pessoa Jurídica
-Console.WriteLine();
-Console.WriteLine("Teste Pessoa Jurídica");
-Console.WriteLine();
+void ControlaSistema()
+{
+    int escolhaUsuario = IniciaPrograma();
+    Console.Clear();
+    Contrato contrato = CriaContrato(escolhaUsuario);
+    contrato.ExibirInfo();
+    EncerraPrograma();
+}
 
-ContratoPessoaJuridica contratoPj1 = new("Lets Pet", 30000, 60, "40.078.334/0001-00", "688.241.069.456");
-contratoPf1.ExibirInfo();
+int IniciaPrograma()
+{
+    Console.WriteLine(@"Sistema de Gestão de Contratos de Financiamento
+
+Gostaria de realizar o cadastro de um contrato de:
+1 - Pessoa física
+2 - Pessoa Jurídica
+");
+    int tipoCadastro = Validacoes.ConsoleIntIntervalo(1, 2);
+    return tipoCadastro;
+}
+
+Contrato CriaContrato(int tipoCadastro)
+{
+    Console.WriteLine("Qual o nome do contratante?");
+    string nomeContratante = Validacoes.ConsoleSemVazio();
+    Console.Clear();
+    Console.WriteLine("Qual o valor do financiamento?");
+    decimal valorContrato = Validacoes.ConsoleDecimalPositivo();
+    Console.Clear();
+    Console.WriteLine("Qual o prazo do financiamento em meses?");
+    int prazoContrato = Validacoes.ConsoleIntIntervalo(1, 240);
+    Console.Clear();
+
+    if (tipoCadastro == 1)
+    {
+        Console.WriteLine("Qual o CPF do contratante?");
+        string cpfDigitado = Validacoes.ConsoleSemVazio();
+        Console.Clear();
+        while (!Validacoes.CpfValido(cpfDigitado))
+        {
+            cpfDigitado = Validacoes.ConsoleSemVazio();
+            Console.Clear();
+        }
+        Console.WriteLine("Qual a data de nascimento do contratante?");
+        DateTime dataNascimento = Validacoes.ConsoleData();
+        Console.Clear();
+        ContratoPessoaFisica contratoPf = new(nomeContratante, valorContrato, prazoContrato, cpfDigitado, dataNascimento);
+
+        return contratoPf;
+    }
+    else
+    {
+        Console.WriteLine("Qual o CNPJ do contratante?");
+        string cnpjDigitado = Validacoes.ConsoleSemVazio();
+        Console.Clear();
+        while (!Validacoes.CnpjValido(cnpjDigitado))
+        {
+            cnpjDigitado = Validacoes.ConsoleSemVazio();
+            Console.Clear();
+        }
+        Console.WriteLine("Qual o número de inscrição estadual do contratante?");
+        string numeroInscricaoDigitado = Validacoes.ConsoleSemVazio();
+        Console.Clear();
+        while (!Validacoes.InscricaoValida(numeroInscricaoDigitado))
+        {
+            numeroInscricaoDigitado = Validacoes.ConsoleSemVazio();
+            Console.Clear();
+        }
+        ContratoPessoaJuridica contratoPj = new(nomeContratante, valorContrato, prazoContrato, cnpjDigitado, numeroInscricaoDigitado);
+        return contratoPj;
+    }
+}
+
+void EncerraPrograma()
+{
+    Console.WriteLine(@"
+
+Deseja Encerrar o programa?
+1 - Sim
+2 - Não");
+    int decisaoEncerramento = Validacoes.ConsoleIntIntervalo(1, 2);
+    if (decisaoEncerramento == 1)
+    {
+        return;
+    }
+    else
+    {
+        Console.Clear();
+        ControlaSistema();
+    }
+}
+
---
+++ Validacoes.cs
@@ -0,0 +1,113 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _854_ExercicioFinanceira_Sergio
+{
+    public static class Validacoes
+    {
+        public static bool CpfValido(string cpf)
+        {
+            Regex RgxCpf = new(@"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$");
+            if (!RgxCpf.Match(cpf).Success)
+            {
+                Console.WriteLine("CPF digitado inválido!\nDigite no formato 123.123.123-12");
+                return false;
+            }
+            return true;
+        }
+        public static bool CnpjValido(string cnpj)
+        {
+            Regex RgxCnpj = new(@"^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}$");
+            if (!RgxCnpj.Match(cnpj).Success)
+            {
+                Console.WriteLine("CNPJ digitado inválido!\nDigite no formato 12.123.123/0001-12");
+                return false;
+            }
+            return true;
+        }
+        public static bool InscricaoValida(string inscricao)
+        {
+            Regex RgxInscricao = new(@"^(\d{3}\.?){3}\d{3}$");
+            if (!RgxInscricao.Match(inscricao).Success)
+            {
+                Console.WriteLine("Inscrição digitada inválida!\nDigite no formato 123.456.789.123");
+                return false;
+            }
+            return true;
+        }
+        public static string ConsoleSemVazio()
+        {
+            bool entradaValida = false;
+            string entradaUsuario = "";
+            while (!entradaValida)
+            {
+                entradaUsuario = Console.ReadLine();
+                if (string.IsNullOrEmpty(entradaUsuario))
+                {
+                    Console.WriteLine("Informação Obrigatória. Digite algo...");
+                }
+                else
+                {
+                    entradaValida = true;
+                }
+            }
+            return entradaUsuario;
+        }
+        public static int ConsoleIntIntervalo(int limteInferior, int limiteSuperior)
+        {
+            int numeroDigitado = 0;
+            bool entradaIntlValida = false;
+            while (!entradaIntlValida)
+            {
+                entradaIntlValida = int.TryParse(ConsoleSemVazio(), out numeroDigitado);
+                if (!entradaIntlValida || numeroDigitado < limteInferior || numeroDigitado > limiteSuperior)
+                {
+                    Console.WriteLine("Digite um número válido...");
+                    entradaIntlValida = false;
+                }
+            }
+            return numeroDigitado;
+        }
+        public static decimal ConsoleDecimalPositivo()
+        {
+            decimal numeroDigitado = 0;
+            bool entradaDecimalValida = false;
+            while (!entradaDecimalValida)
+            {
+                entradaDecimalValida = decimal.TryParse(ConsoleSemVazio(), out numeroDigitado);
+                if (!entradaDecimalValida || numeroDigitado <= 0)
+                {
+                    Console.WriteLine("Digite um número válido...");
+                    entradaDecimalValida = false;
+                }
+            }
+            return numeroDigitado;
+        }
+        public static DateTime ConsoleData()
+        {
+            DateTime dataDigitada = DateTime.MinValue;
+            bool entradaDataValida = false;
+
+            //Regex Data
+            // \b([0-2]{0,1}[1-9]|(3)[0-1])(\/)(((0){0,1}[1-9])|((1)[0-2]))(\/)\d{4}\b
+
+            while (!entradaDataValida)
+            {
+                entradaDataValida = DateTime.TryParse(ConsoleSemVazio(), out dataDigitada);
+                if (!entradaDataValida)
+                {
+                    Console.WriteLine("Digite uma data válida no formato dd-mm-aaaa...");
+                }
+                if (dataDigitada >= DateTime.Now)
+                {
+                    Console.WriteLine("Data deve estar no passado...");
+                    entradaDataValida = false;
+                }
+            }
+            return dataDigitada;
+        }
+        public static bool VerificaMaioridade(int idade)
+        {
+            return idade >= 18;
+        }
+    }
+}
